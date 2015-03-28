7d63405c5dd0b4bcd29f733c8214a4df2c1de53d
derwolfe <chriswwolfe@gmail.com>
2015-03-28 13:45:15 -0500

    use react, start working on query

--- src/main.py
+++ src/main.py
@@ -10,23 +10,23 @@ from sqlalchemy.schema import CreateTable
 from twisted.internet import reactor, defer
 from twisted.internet.task import react
 
+from twisted.python.filepath import FilePath
 
+
+#PROJECT_PATH = FilePath(__file__).dirname()
+#print(PROJECT_PATH)
 Engine = create_engine(
         "sqlite://", reactor=reactor, strategy=TWISTED_STRATEGY
 )
 Metadata = MetaData(Engine)
 Users = Table("users", Metadata,
-            Column("id", Integer(), primary_key=True),
-            Column("name", String()),
+              Column("id", Integer(), primary_key=True),
+              Column("name", String()),
+              keep_existing=True
         )
 
-
 def setupDb(_engine):
-    d = _engine.execute(CreateTable(Users))
-    def exists(ignored):
-        print("exists?")
-    d.addCallback(exists)
-    return d
+    return _engine.execute(CreateTable(Users))
 
 def addUsers(result, _engine):
     newUsers = [
@@ -38,28 +38,29 @@ def addUsers(result, _engine):
     ]
     return _engine.execute(Users.insert().values(newUsers))
 
-def getDUsers(_result, _engine):
+
+def getUsersStartingWith(_result, _engine, queryLetter):
     d = _engine.execute(
-        Users.select(Users.c.name.startswith("D"))
+        Users.select(Users.c.name.startswith(queryLetter))
     )
     def realize(results):
         return results.fetchall()
     d.addCallback(realize)
     return d
 
+
 def printResults(users):
     for user in users:
         print("Username: %s" % user[Users.c.name])
 
+
 def main(reactor):
     d = setupDb(Engine)
     d.addCallback(addUsers, _engine=Engine)
-    d.addCallback(getDUsers, _engine=Engine)
+    d.addCallback(getUsersStartingWith, _engine=Engine, queryLetter="J")
     d.addCallback(printResults)
     return d
 
 
 if __name__ == "__main__":
-    #main(reactor)
-    #reactor.run()
     react(main, [])
