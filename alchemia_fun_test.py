5f13f2b040f7a43d4193a7b3c6fd8dbe76e62da9
derwolfe <chriswwolfe@gmail.com>
2015-03-29 21:32:22 -0500

    test file

---
+++ alchemia_fun_test.py
@@ -0,0 +1,34 @@
+from __future__ import print_function, absolute_import, division
+
+from twisted.trial import unittest
+from alchemia_fun import (
+    Database,
+    SearchCommandProtocol,
+    connectionString,
+    engine,
+    metadata,
+    users
+)
+
+class TestDatabase(unittest.TestCase):
+
+    def setUp(self):
+        self.db = Database()
+
+    def test_setupCreatesUsersTable(self):
+        """
+        setup creates a table named 'users'
+        """
+        d = self.db.setup()
+        def check(_):
+            result = engine.has_table(connectionString, 'users')
+            self.assertTrue(result)
+        d.addCallback(check)
+        return d
+
+
+    def test_setupAddsNewUsers(self):
+        """
+        Users are added to the database.
+        """
+        pass
